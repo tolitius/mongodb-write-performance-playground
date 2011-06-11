a8fe9e9485a1327ac5e45aaa9f3502a0bc07ae4b
anatoly-polinsky <tolitius@gmail.com>
2011-06-11 14:03:47 -0400

    reading multiple host YAML config to achieve true multi hosting ( rather then localhost:[multi porting] )

--- java/src/main/java/org/dotkam/mongodb/benchmark/MultipleHostMongoKiller.java
+++ java/src/main/java/org/dotkam/mongodb/benchmark/MultipleHostMongoKiller.java
@@ -2,40 +2,64 @@ package org.dotkam.mongodb.benchmark;
 
 
 import org.dotkam.document.VeryImportantDocument;
+import org.dotkam.mongodb.concurrent.MongoMultipleHostDocumentWriter;
 import org.dotkam.mongodb.concurrent.MongoSingleHostDocumentWriter;
 import org.dotkam.mongodb.datasource.CollectionDataSource;
 import org.dotkam.mongodb.partition.GridSizeDocumentPartitioner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
- * <p> Quick and dirty way to "be killing" a single MongoS / MongoD pair </p>
+ * <p> Quick and dirty way to "be killing" multiple MongoS / MongoD pairs </p>
  */
 public class MultipleHostMongoKiller extends AbstractMongoKiller {
 
     private final static Logger logger = LoggerFactory.getLogger( MultipleHostMongoKiller.class );
 
-    public MultipleHostMongoKiller(Map<String, Object> config) {
+    public MultipleHostMongoKiller( Map<String, Object> config ) {
 
         super.numberOfDocuments = Long.valueOf( config.get( "number_of_documents" ).toString() );
-        super.gridSize = Integer.valueOf( config.get("grid_size").toString() );
-        super.batchThreshold = Integer.valueOf(config.get("single_batch_threshold").toString());
+        super.gridSize = Integer.valueOf( config.get( "grid_size" ).toString() );
+        super.batchThreshold = Integer.valueOf(config.get( "single_batch_threshold" ).toString());
         super.uniqueSalt = new Date().getTime();
 
-        CollectionDataSource dataSource =
-                new CollectionDataSource( config.get( "db_name" ).toString(),
-                                          config.get( "collection_name" ).toString() );
+        List<CollectionDataSource> multipleHosts = this.createHostDataSources( config );
 
-        super.documentWriter = new MongoSingleHostDocumentWriter(  VeryImportantDocument.class,
-                                                   new GridSizeDocumentPartitioner(),
-                                                   dataSource,
-                                                   super.gridSize );
+        super.documentWriter = new MongoMultipleHostDocumentWriter(  VeryImportantDocument.class,
+                                                                     new GridSizeDocumentPartitioner(),
+                                                                     multipleHosts,
+                                                                     super.gridSize );
     }
 
     public void destroyTarget() {
         super.doDestroyTarget();
+    }
+
+    private List<CollectionDataSource> createHostDataSources ( Map<String, Object> config ) {
+
+        List<CollectionDataSource> multipleHosts = new ArrayList<CollectionDataSource>();
+
+        System.out.println( config.get("hosts").toString() );
+        List<Map<String, Object>> hosts = ( List ) config.get( "hosts" );
+        super.numberOfHosts = hosts.size();
+
+        for ( int i = 0; i < super.numberOfHosts; i++) {
+
+            CollectionDataSource host = new CollectionDataSource(
+                    config.get( "db_name" ).toString(),
+                    config.get( "collection_name" ).toString(),
+                    hosts.get( i ).get( "name" ).toString(),
+                    Integer.valueOf( hosts.get( i ).get( "port" ).toString() ) );
+
+            //host.getCollection().drop();
+            multipleHosts.add( host );
+        }
+
+        return multipleHosts;
     }
 }
